343e2e0384e10530aa4b2ea802580beddaae2fc9
Alex Beeston <alexmbeeston@gmail.com>
2021-03-07 03:36:38 -0800

    clicks next arrow until there are not more pages

--- Scrapper/Program.cs
+++ Scrapper/Program.cs
@@ -1,18 +1,51 @@
 ﻿using System;
-
+using System.Collections.Generic;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
+using System.Threading;
 
 namespace Scrapper
 {
 	class Program
 	{
+		const int WAIT_TIME = 15000;
 		static void Main()
 		{
 			IWebDriver driver = new FirefoxDriver();
-			driver.Navigate().GoToUrl("https://usu.edu");
-			Console.WriteLine(driver.Title);
+			string sampleSeed = "https://www.kohls.com/catalog.jsp?CN=Gender:Mens+Silhouette:Button-Down Shirts+Category:Tops+Department:Clothing";
+			List<string> seeds = new List<string>
+			{
+				sampleSeed
+			};
+			foreach (string seed in seeds) ProcessSeed(seed, driver);
+		}
+
+		static void ProcessSeed(string seed, IWebDriver driver)
+		{
+			driver.Navigate().GoToUrl(seed);
+			Thread.Sleep(WAIT_TIME);
+			do
+			{
+				ProcessPage(driver);
+			} while (ClickNextArrow(driver));
+		}
+
+		static void ProcessPage(IWebDriver driver)
+		{
+
+		}
+
+		static bool ClickNextArrow(IWebDriver driver)
+		{
+			IWebElement nextArrow = driver.FindElement(By.CssSelector(".nextArw"));
+			if (nextArrow.Displayed)
+			{
+				nextArrow.Click();
+				Thread.Sleep(WAIT_TIME);
+				return true;
+			}
+			else return false;
 		}
 	}
 }
