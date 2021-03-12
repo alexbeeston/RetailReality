5989acb2ba3d17d2a7fb4bdafeac0c65cdd4ee6c
Alex Beeston <alexmbeeston@gmail.com>
2021-03-12 01:16:24 -0700

    Introduce SetTerminateProcessOnEnter Task to cleanly release resources acquired by the driver.

--- Scrapper/Program.cs
+++ Scrapper/Program.cs
@@ -3,43 +3,66 @@ using System.Collections.Generic;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
-using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
 
 namespace Scrapper
 {
 	class Program
 	{
-		const int WAIT_TIME = 15000;
 		static void Main()
 		{
+			Directory.SetCurrentDirectory(@"..\..\..\");
+			StreamWriter file = File.CreateText("data.csv");
+			file.WriteLine("datetime,id,stars,reviews,label_1,price_1,price_2");
+
 			IWebDriver driver = new FirefoxDriver();
-			string sampleSeed = "https://www.kohls.com/catalog.jsp?CN=Gender:Mens+Silhouette:Button-Down Shirts+Category:Tops+Department:Clothing";
+			SetTerminateProcessOnEnter(driver);
 			List<string> seeds = new List<string>
 			{
-				sampleSeed
+				"https://www.kohls.com/catalog.jsp?CN=Gender:Mens+Product:Pants+Category:Bottoms+Department:Clothing",
+				"https://www.kohls.com/catalog.jsp?CN=Gender:Womens+Product:Jeans+Category:Bottoms+Department:Clothing",
+				"https://www.kohls.com/catalog.jsp?CN=Gender:Mens+Silhouette:Button-Down Shirts+Category:Tops+Department:Clothing",
+				"https://www.kohls.com/catalog.jsp?CN=Gender:Mens+Product:Shorts+Category:Bottoms+Department:Clothing"
 			};
-			foreach (string seed in seeds) ProcessSeed(seed, driver);
+			foreach (string seed in seeds) ProcessSeed(seed, driver, file);
+			driver.Quit();
 		}
 
-		static void ProcessSeed(string seed, IWebDriver driver)
+		static void ProcessSeed(string seed, IWebDriver driver, StreamWriter file)
 		{
 			driver.Navigate().GoToUrl(seed);
 			do
 			{
 				Console.WriteLine("New Page");
-				ProcessPage(driver, DateTime.Now.ToUniversalTime());
+				ProcessPage(driver, DateTime.Now.ToUniversalTime(), file, true);
 			} while (ClickNextArrow(driver));
 		}
 
-		static void ProcessPage(IWebDriver driver, DateTime requestTime)
+		static void ProcessPage(IWebDriver driver, DateTime requestTime, StreamWriter file, bool write = false)
 		{
+			Console.WriteLine("Waiting...");
+			WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+			Console.WriteLine("Done Waiting");
 			IReadOnlyList<IWebElement> products = driver.FindElements(By.CssSelector(".product-description"));
+			Console.Write($"Length of products: {products.Count}");
 			foreach (IWebElement product in products)
 			{
+				//file.Write(requestTime.ToLocalTime().ToString() + ",");
+
 				// product ID
-				string id = product.GetAttribute("id");
-				id = id.Remove(id.IndexOf('_'));	
-				Console.WriteLine("  " + id);
+				string id;
+				try
+				{
+					id = product.GetAttribute("id");
+					id = id.Remove(id.IndexOf('_'));	
+				}
+				catch (Exception e)
+				{
+					id = e.ToString();
+				}
+				//file.Write(id + ",");
 
 				// stars
 				string stars;
@@ -52,7 +75,11 @@ namespace Scrapper
 				{
 					stars = "-1";
 				}
-				Console.WriteLine(stars);
+				catch (Exception e)
+				{
+					stars = e.ToString();
+				}
+				//file.Write(stars + ",");
 
 				// reviews
 				string reviews;
@@ -64,20 +91,35 @@ namespace Scrapper
 				{
 					reviews = "-1";
 				}
-				Console.WriteLine(reviews);
+				catch (Exception e)
+				{
+					reviews = e.ToString();
+				}
+				//file.Write(reviews + ",");
 
-				// first label
+				// label 1
 				string firstLabel = product.FindElement(By.CssSelector(".prod_price_label")).Text;
-				Console.WriteLine(firstLabel);
+				//file.Write(firstLabel + ",");
 
-				// first price
+				// price 1
 				string firstPrice = product.FindElement(By.CssSelector(".prod_price_amount")).Text;
-				Console.WriteLine(firstPrice);
+				//file.Write(firstPrice + ",");
 
-				// second price
+				// price 2
 				string secondPrice = product.FindElement(By.CssSelector(".prod_price_original")).Text;
-				Console.WriteLine(secondPrice);
+				//file.Write(secondPrice + "\n");
+				//file.Flush();
 
+				// debugging
+				if (write)
+				{
+					Console.WriteLine(id);
+					Console.Write("  " + stars);
+					Console.Write("  " + reviews);
+					Console.Write("  " + firstLabel);
+					Console.Write("  " + firstPrice);
+					Console.WriteLine("  " + secondPrice);
+				}
 			}
 		}
 
@@ -90,6 +132,17 @@ namespace Scrapper
 				return true;
 			}
 			else return false;
+		}
+		
+		static void SetTerminateProcessOnEnter(IWebDriver driver)
+		{
+			Task.Run(() => {
+				Console.ReadLine();
+				Console.WriteLine("Terminating process");
+				driver.Quit();
+				Console.WriteLine("Just quit the driver");
+				Environment.Exit(0);
+			});
 		}
 	}
 }
