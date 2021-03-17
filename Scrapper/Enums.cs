8b55005df1cd745c53c02a2709c13569b142c42a
Alex Beeston <alexmbeeston@gmail.com>
2021-03-17 07:56:00 -0600

    added starter methods for parsing text and returning PriceInformants for price 1 and price 2. Added DataBaseNotes.md; now going to add explicit waits to click through the pages with fewer errors (hopefully none), then log all the different prices types I get before starting to try and parse them into Price Informants.

---
+++ Scrapper/Enums.cs
@@ -0,0 +1,22 @@
+﻿namespace Scrapper
+{
+	enum Label
+	{
+		Original,
+		Sale,
+		Regular,
+		Clearance,
+		Group,
+		None,
+		Unknown	
+	}
+
+	enum PriceType
+	{
+		Single, // $15
+		Range, // $15 - $20
+		Hybrid, // $15 or 2/$25
+		Bulk, // 2 / $25
+		Unknown
+	}
+}
--- Scrapper/Program.cs
+++ Scrapper/Program.cs
@@ -21,14 +21,10 @@ namespace Scrapper
 			if (logData) file.WriteLine("id,stars,reviews,firstLabel,firstPrice,secondPrice");
 
 			IWebDriver driver = new ChromeDriver();
-			driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
 			SetTerminateProcessOnEnter(driver);
 			List<string> seeds = new List<string>
 			{
-				//"https://www.kohls.com/catalog.jsp?CN=Gender:Mens+Product:Pants+Category:Bottoms+Department:Clothing",
-				//"https://www.kohls.com/catalog.jsp?CN=Gender:Womens+Product:Jeans+Category:Bottoms+Department:Clothing",
-				//"https://www.kohls.com/catalog.jsp?CN=Gender:Mens+Silhouette:Button-Down Shirts+Category:Tops+Department:Clothing",
-				"https://www.kohls.com/catalog.jsp?CN=Gender:Mens+Product:Shorts+Category:Bottoms+Department:Clothing"
+				"https://www.kohls.com/catalog/mens-tops-tees-tops-clothing.jsp?CN=Gender:Mens+Product:Tops%20%26%20Tees+Category:Tops+Department:Clothing&cc=mens-TN3.0-S-shirtstees&kls_sbp=34312085268895173668447991324068535941"
 			};
 			foreach (string seed in seeds) ProcessSeed(seed, driver, file);
 			driver.Quit();
@@ -40,53 +36,49 @@ namespace Scrapper
 			driver.Navigate().GoToUrl(seed);
 			do
 			{
-				ProcessPage(driver, DateTime.Now.ToUniversalTime(), file, true);
+				ProcessPage(driver, DateTime.Now.ToUniversalTime(), file);
 			} while (ClickNextArrow(driver));
 		}
 
-		static void ProcessPage(IWebDriver driver, DateTime requestTime, StreamWriter file, bool write = false)
+		static void ProcessPage(IWebDriver driver, DateTime requestTime, StreamWriter file)
 		{
+			System.Threading.Thread.Sleep(10000); // todo: move to explicit wait
 			IReadOnlyList<IWebElement> products = driver.FindElements(By.CssSelector(".product-description"));
-			string nullIndicator = "null";
 			foreach (IWebElement product in products)
 			{
 				string id = product.GetAttribute("id");
 				id = id.Remove(id.IndexOf('_'));
-
 				string stars = SafeFindElement(product, ".stars")?.GetAttribute("title").Trim();
-				stars = stars?.Remove(stars.IndexOf(' ')) ?? nullIndicator;
-
-				string reviews = SafeFindElement(product, ".prod_ratingCount")?.Text.TrimStart('(').TrimEnd(')') ?? nullIndicator;
-
-				string firstLabel = SafeFindElement(product, ".prod_price_label")?.Text ?? nullIndicator;
-
-				string firstPrice = SafeFindElement(product, ".prod_price_amount")?.Text ?? nullIndicator;
-
-				string secondPrice = product.FindElement(By.CssSelector(".prod_price_original")).Text ?? nullIndicator;
+				stars = stars?.Remove(stars.IndexOf(' ')) ?? null;
+				string reviews = SafeFindElement(product, ".prod_ratingCount")?.Text.TrimStart('(').TrimEnd(')') ?? null;
+				string price1LabelRaw = SafeFindElement(product, ".prod_price_label")?.Text ?? null;
+				string price1AmountRaw = SafeFindElement(product, ".prod_price_amount")?.Text ?? null;
+				string price2Raw = product.FindElement(By.CssSelector(".prod_price_original")).Text ?? null;
 
 				// debugging
 				Console.WriteLine(id);
 				Console.Write("  " + stars);
 				Console.Write("  " + reviews);
-				Console.Write("  " + firstLabel);
-				Console.Write("  " + firstPrice);
-				Console.WriteLine("  " + secondPrice);
+				Console.Write("  " + price1LabelRaw);
+				Console.Write("  " + price1AmountRaw);
+				Console.WriteLine("  " + price2Raw);
 
 				// logging
 				if (logData) file.Write(id + ",");
 				if (logData) file.Write(stars + ",");
 				if (logData) file.Write(reviews + ",");
-				if (logData) file.Write(firstLabel + ",");
-				if (logData) file.Write(firstPrice + ",");
-				if (logData) file.Write(secondPrice + "\n");
+				if (logData) file.Write(price1LabelRaw + ",");
+				if (logData) file.Write(price1AmountRaw + ",");
+				if (logData) file.Write(price2Raw + "\n");
 				if (logData) file.Flush();
 			}
 		}
-		
+	
 		static IWebElement SafeFindElement(IWebElement element, string cssSelector)
 		{
 			try
 			{
+				// todo: explicit wait
 				return element.FindElement(By.CssSelector(cssSelector));
 			}
 			catch (NoSuchElementException)
@@ -95,8 +87,75 @@ namespace Scrapper
 			}
 		}
 
+		static PriceInformant ProcessPrice1(string rawLabel, string rawAmount)
+		{
+			rawLabel = rawLabel?.Trim().ToLower() ?? null;
+			Label labelType;	
+			switch (rawLabel)
+			{
+				case "original":
+					labelType = Label.Original;
+					break;
+				case "sale":
+					labelType = Label.Sale;
+					break;
+				case "regular":
+					labelType = Label.Regular;
+					break;
+				case "reg.":
+					labelType = Label.Regular;
+					break;
+				case "clearance":
+					labelType = Label.Clearance;
+					break;
+				case "group":
+					labelType = Label.Group;
+					break;
+				case "":
+					labelType = Label.None;
+					break;
+				case null:
+					labelType = Label.Unknown;
+					break;
+				default:
+					labelType = Label.Unknown;
+					break;
+			}
+
+			PriceType priceType = PriceType.Hybrid;
+			float amount = 10;
+			if (rawAmount == null)
+			{
+				priceType = PriceType.Unknown;
+				amount = -1;
+			}
+			else if (rawAmount.Contains("or"))
+			{
+				if (logData) file.Write($"<<<<<<< detecting a hybrid price for {rawAmount}.");
+				priceType = PriceType.Hybrid;
+				const int expectedElements = 2;
+				string[] heyMan = rawAmount.Split("or", expectedElements);
+			}
+			else if (rawAmount.Contains('-'))
+			{
+				priceType = PriceType.Hybrid;
+			}
+			else
+			{
+				priceType = PriceType.Single;
+			}
+
+			return new PriceInformant(priceType, labelType, amount);
+		}
+
+		static PriceInformant ProcessPrice2(string rawText)
+		{
+			return new PriceInformant();
+		}
+
 		static bool ClickNextArrow(IWebDriver driver)
 		{
+			// todo: explicit wait
 			IWebElement nextArrow = driver.FindElement(By.CssSelector(".nextArw"));
 			if (nextArrow.Displayed)
 			{
@@ -118,4 +177,19 @@ namespace Scrapper
 			});
 		}
 	}
+
+	struct PriceInformant
+	{
+		public PriceType type;
+		public Label label;
+		public float amount;
+
+		public PriceInformant(PriceType type, Label label, float amount)
+		{
+			this.type = type;
+			this.label = label;
+			this.amount = amount;
+		}
+	}
+
 }
