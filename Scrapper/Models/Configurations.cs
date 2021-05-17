a077d5122219e2f22f06ad8dbfe923065da3b133
Alex Beeston <alexmbeeston@gmail.com>
2021-05-16 21:10:23 -0600

    forgot straggling files in last commit

---
+++ Scrapper/Models/Configurations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scrapper
+{
+	class Configurations
+	{
+		public bool writeOffersToConsole;
+		public bool logOffersToCsv;
+		public bool writeScrapResultsToStatus;
+		public int pagesToScrap;
+		public string mySqlUserName;
+		public string mySqlPassword;
+		public string mySqlHostIp;
+		public uint mySqlTimeout;
+		public List<Combination> combinations = new List<Combination>();
+		public List<Pair> pairs = new List<Pair>();
+
+		// could probably delete after dev
+		public bool doAsync;
+		public bool isDataBaseDevEnv;
+	}
+	
+	class Combination
+	{
+		public int id;
+		public List<int> include;
+	}
+
+	class Pair
+	{
+		public int id;
+		public string key;
+		public string value;
+	}
+}
---
+++ Scrapper/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scrapper
+{
+	class Product
+	{
+		public string id;
+		public string name;
+		public string brand;
+		public Dictionary<string, string> searchParameters;
+
+		public Product(string id, string name, Dictionary<string, string> searchParameters)
+		{
+			this.id = id;
+			this.name = name;
+			this.searchParameters = searchParameters;
+			brand = "figure out how to get branch from title";
+		}
+	}
+}
